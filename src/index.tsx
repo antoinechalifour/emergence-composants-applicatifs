734b1bf1a4c38229081a23939261ba510dd87186
Antoine Chalifour <antoine.chalifour@octo.com>
2021-03-26 16:37:05 +0100

    fix: codesandbox

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,7 @@ import "./styles.css";
 import { render } from "react-dom";
 
 import { Chat } from "./components/Chat-after-refactoring";
+import { MessagerieFake } from "./model/MessagerieFake";
 
 const rootElement = document.getElementById("root");
-render(<Chat />, rootElement);
+render(<Chat messagerie={new MessagerieFake()} />, rootElement);
